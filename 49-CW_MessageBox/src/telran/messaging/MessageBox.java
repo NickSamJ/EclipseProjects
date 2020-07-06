0167aa62d42c6f2000b02bbff9fdb3e4cedbfdb5
NickSamJ <samokhin1j@gmail.com>
2020-07-06 18:03:12 +0300

    Offline like lessons 50, 51

--- 49-CW_MessageBox/src/telran/messaging/MessageBox.java
+++ 49-CW_MessageBox/src/telran/messaging/MessageBox.java
@@ -5,17 +5,32 @@ public class MessageBox {
 
 	public void put(String message) {
 		synchronized (this) {
+			while(this.message != null) {
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 			this.message = message;
-			this.notify();
+			this.notifyAll();
 		}
 	}
 	
 	public String take() throws InterruptedException {
 		synchronized (this) {
-//			while(message == null) {
-			if(message == null) {
+			while(message == null) {
 				this.wait();
 			}
+			String res = message;
+			message = null;
+			this.notify();
+			return res;
+		}
+	}
+	
+	public String get() {
+		synchronized (this) {
 			String res = message;
 			message = null;
 			return res;
